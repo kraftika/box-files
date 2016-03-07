6188e4ec067becb830f33016f256b318e69221f8
Ionut Pistol <ipistol@adobe.com>
2016-03-07 10:21:14 +0200

    Added and configured yeoman package for development and test cases

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boxFilesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the boxFilesApp
+ */
+angular.module('boxFilesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boxFilesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the boxFilesApp
+ */
+angular.module('boxFilesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
